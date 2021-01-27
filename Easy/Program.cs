8a5c2eb7dad87464b886a475cac9b78d6f885a51
Jagath <jagath.chandra@kanini.com>
2021-01-27 21:22:31 +0530

    Add .gitignore and .gitattributes.

---
+++ Easy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Employees = new List<string>();
+            Console.WriteLine("Please enter the Employee namesin the order of their eligibility for promotion(Please enter blank to stop)");
+            for (; ; )
+            {
+                String s = Console.ReadLine();
+                if (s == "")
+                    break;
+                else
+                    Employees.Add(s);
+            }
+            
+            
+
+            Console.WriteLine("Please enter the name of the employee to check promotion position");
+            string name = Console.ReadLine();
+            int index = Employees.IndexOf(name) + 1;
+            Console.WriteLine(name+" is the position "+(index)+ " for promotion");
+
+
+
+            Console.WriteLine("The current size of the collection is " +Employees.Capacity);
+            Employees.TrimExcess();
+            Console.WriteLine("The size after removing the extra space is " +Employees.Capacity);
+
+         
+
+
+
+            Console.WriteLine("Promoted employee list:");
+            Employees.Sort();
+            Employees.ForEach(Console.WriteLine);
+
+
+            
+
+        }
+
+    }
+}
